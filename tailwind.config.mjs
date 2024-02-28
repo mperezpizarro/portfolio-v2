73ef91167cdb248cd3de240d5341c72299cbbc75
mperezpizarro <marioperezpizarro@gmail.com>
2024-02-28 16:36:06 +0100

    Added new colours to config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundColor: {
+				"cGray": "#2e2e2e"
+			},
+			borderColor: {
+				"cGray": "#4e4e4e"
+			}
+		},
 	},
 	plugins: [
 		require('tailwindcss-animated')
