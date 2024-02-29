11ca7b805e5377068584f4a7d07b052686524193
mperezpizarro <marioperezpizarro@gmail.com>
2024-02-29 15:25:36 +0100

    Fixed mobile menu icon

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -23,11 +23,11 @@
     </ul>
 
     <button class="flex justify-end w-full md:hidden text-xl text-slate-100" @click="toggleVisible">
-        <svg class="inline-block" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z"/></svg> 
+        <svg class="inline-block" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M20 7H4m16 5H4m16 5H4"/></svg>
     </button>
 
     <div class="block md:hidden relative">
-        <ul v-show="isVisible" class="flex flex-col bg-cGray w-full h-screen fixed z-20 inset-0 py-6 px-4 gap-y-2">
+        <ul class="flex flex-col transition animate-fade animate-duration-100 bg-cGray w-full h-screen fixed z-20 inset-0 py-6 px-4 gap-y-2" :class="isVisible ? 'translate-y-0' : 'translate-x-[-200%]'">
             <button class="flex justify-end w-full text-slate-100" @click="toggleVisible">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z"/></svg>
             </button>
